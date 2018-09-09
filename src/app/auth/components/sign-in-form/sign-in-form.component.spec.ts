4673eaf05c8d2ea8f23cbf1c3530f2dcd8bd4e93
Tim <tja2353@gmail.com>
2018-09-09 16:29:55 +0100

    snapshot works!

--- src/app/auth/components/sign-in-form/sign-in-form.component.spec.ts
+++ src/app/auth/components/sign-in-form/sign-in-form.component.spec.ts
@@ -1,22 +1,30 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SignInFormComponent } from '@app/auth/components/sign-in-form/sign-in-form.component';
 import { IonicModule } from '@ionic/angular';
+
+import { SignInFormComponent } from '@app/auth/components/sign-in-form/sign-in-form.component';
+
 // import { combineReducers, Store, StoreModule } from '@ngrx/store';
 
 describe('Sign In Page', () => {
   let fixture: ComponentFixture<SignInFormComponent>;
-  let instance: SignInFormComponent;
+  let component: SignInFormComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [IonicModule.forRoot(), ReactiveFormsModule],
-      declarations: [ SignInFormComponent ],
+      imports: [IonicModule, ReactiveFormsModule],
+      declarations: [SignInFormComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignInFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   /*
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -28,12 +36,16 @@ describe('Sign In Page', () => {
     fixture = TestBed.createComponent(SignInFormComponent);
     instance = fixture.componentInstance;
   });
+  */
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
   it('should compile', () => {
     // fixture.detectChanges();
-    // expect(fixture).toMatchSnapshot();
+    expect(fixture).toMatchSnapshot();
   });
-*/
 
   it('dummy', () => {
     expect(1).toEqual(1);
