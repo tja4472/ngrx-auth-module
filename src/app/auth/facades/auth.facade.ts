974fe385c70f7b4707b32021224b8d61d7fe3bc7
Tim <tja2353@gmail.com>
2019-03-31 11:09:51 +0100

    tidy auth.facade

--- src/app/auth/facades/auth.facade.ts
+++ src/app/auth/facades/auth.facade.ts
@@ -1,42 +1,46 @@
 import { Injectable } from '@angular/core';
 
 import { Action, select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
-import {
-  AuthApiActions,
-  SignInPageActions,
-  SignUpPageActions,
-} from '@app/auth/actions';
-
-import { Credentials } from '@app/auth/models/credentials.model';
-import { State } from '@app/auth/reducers';
 import { authQuery } from '@app/auth/selectors/auth.selectors';
 import { signInPageQuery } from '@app/auth/selectors/sign-in-page.selectors';
 import { signUpPageQuery } from '@app/auth/selectors/sign-up-page.selectors';
+import { UserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthFacade {
-  public authUser$ = this.store.pipe(select(authQuery.selectAuthUser));
+  public authUser$: Observable<UserModel>;
 
-  public signInPageError$ = this.store.pipe(
-    select(signInPageQuery.selectSignInPageError)
-  );
+  public signInPageError$: Observable<string>;
 
-  public signInPagePending$ = this.store.pipe(
-    select(signInPageQuery.selectSignInPagePending)
-  );
+  public signInPagePending$: Observable<boolean>;
 
-  public signUpPageError$ = this.store.pipe(
-    select(signUpPageQuery.selectSignUpPageError)
-  );
+  public signUpPageError$: Observable<string>;
 
-  public signUpPagePending$ = this.store.pipe(
-    select(signUpPageQuery.selectSignUpPagePending)
-  );
+  public signUpPagePending$: Observable<boolean>;
 
-  constructor(private store: Store<{}>) {}
+  constructor(private store: Store<{}>) {
+    this.authUser$ = store.pipe(select(authQuery.selectAuthUser));
+
+    this.signInPageError$ = store.pipe(
+      select(signInPageQuery.selectSignInPageError)
+    );
+
+    this.signInPagePending$ = store.pipe(
+      select(signInPageQuery.selectSignInPagePending)
+    );
+
+    this.signUpPageError$ = store.pipe(
+      select(signUpPageQuery.selectSignUpPageError)
+    );
+
+    this.signUpPagePending$ = store.pipe(
+      select(signUpPageQuery.selectSignUpPagePending)
+    );
+  }
 
   public dispatch(action: Action): void {
     this.store.dispatch(action);
